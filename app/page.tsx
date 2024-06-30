339415dfb702d457f5675bd7e6f51299b1d1d512
Okoye <okoyedivine145@gmail.com>
2024-06-30 14:34:12 +0100

    dynamic routing

--- app/page.tsx
+++ app/page.tsx
@@ -9,24 +9,48 @@ import blur2 from "@/app/assets/home_page_blur_2.png";
 export default function Home() {
   return (
     <div
-    className="flex w-full flex-col px-56 py-8 items-center justify-center relative"
-    // className="flex w-full flex-col items-center justify-center gap-4 rounded-[20px] border border-border bg-blue/5 p-10 text-2xl font-semibold"
+      className="relative flex w-full flex-col items-center justify-center px-4 py-8 sm:px-10 lg:px-56"
+      // className="flex w-full flex-col items-center justify-center gap-4 rounded-[20px] border border-border bg-blue/5 p-10 text-2xl font-semibold"
     >
-      <Image className="absolute left-16 top-4" src={blur1} alt="Blur" height={400}/>
-      <Image className="absolute right-16" src={blur2} alt="Blur" height={400}/>
-      <h1 className="text-6xl text-center font-semibold">Start using BasePay for <span className="text-[#3B72FF]">Crypto</span> payments.</h1>
-
+      <Image
+        className="absolute left-16 top-4"
+        src={blur1}
+        alt="Blur"
+        height={400}
+      />
+      <Image
+        className="absolute right-16"
+        src={blur2}
+        alt="Blur"
+        height={400}
+      />
+      <h1 className="text-center text-6xl font-semibold">
+        Start using BasePay for <span className="text-[#3B72FF]">Crypto</span>{" "}
+        payments.
+      </h1>
       <br /> <br />
-      <p className="text-center font-medium">BasePay is a groundbreaking payment gateway that revolutionizes the way you handle transactions with cryptocurrencies. It seamlessly connects the tried-and-true world of traditional finance with the dynamic, ever-evolving realm of digital currencies.</p>
-
-
+      <p className="text-center font-medium">
+        BasePay is a groundbreaking payment gateway that revolutionizes the way
+        you handle transactions with cryptocurrencies. It seamlessly connects
+        the tried-and-true world of traditional finance with the dynamic,
+        ever-evolving realm of digital currencies.
+      </p>
       <Link href="/hackathon-payment">
         <Button className="mt-16 w-96">Connect wallet</Button>
       </Link>
-
       <br />
-      <small className="font-medium w-96 text-center">Don&apos;t have a wallet yet? We&apos;ll create one for you on BasePay.</small>
-      <small className="font-medium w-96 text-center"><a href="" className="text-[#3B72FF] underline font-bold">Click here</a> to create a new one.</small>
+      <small className="w-96 text-center font-medium">
+        Don&apos;t have a wallet yet? We&apos;ll create one for you on BasePay.
+      </small>
+      <small className="w-96 text-center font-medium">
+        <Link
+          href="/hackathon-payment"
+          className="font-bold text-[#3B72FF] underline"
+        >
+          Click here
+        </Link>
+        to create a new one.
+      </small>
     </div>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, {FC} from "react";
+import React, { FC } from "react";
 import logo from "@/app/assets/logo.png";
 import menu from "@/app/assets/menu.svg";
 import profilePic from "@/app/assets/profile-pic.svg";
@@ -12,14 +12,13 @@ import DisconnectModal from "./DisconnectModal";
 interface IProps {}
 
 const Header: FC<IProps> = () => {
-
   return (
-    <header className="sticky backdrop-blur-[3px] top-0 z-50 mb-8 w-[96%] mx-[2%] px-6 py-4 border-b-2 flex items-center justify-center">
-			<Link href="/">
-				<Image src={logo} alt="logo" width={150} height={40} />
-			</Link>
-			{/* <span className="absolute left-0 top-16 my-4 flex h-[2px] w-full bg-border" /> */}
-		</header>
+    <header className="sticky top-0 z-50 mx-[2%] mb-8 flex w-[96%] items-center justify-center border-b-2 bg-white/80 px-6 py-4 backdrop-blur-[3px]">
+      <Link href="/">
+        <Image src={logo} alt="logo" width={150} height={40} />
+      </Link>
+      {/* <span className="absolute left-0 top-16 my-4 flex h-[2px] w-full bg-border" /> */}
+    </header>
   );
 };
 
